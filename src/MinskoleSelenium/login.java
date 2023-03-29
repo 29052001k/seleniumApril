dcda87145797252185366630137ef7648a793012
29052001k <kalyanihadole2001@gmail.com>
2023-03-29 08:38:19 +0530

    added

---
+++ src/MinskoleSelenium/login.java
@@ -0,0 +1,15 @@
+package MinskoleSelenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hello\\Desktop\\chromedriver\\chromedriver.exe");
+		
+		ChromeDriver driver=new ChromeDriver();
+	}
+
+}
